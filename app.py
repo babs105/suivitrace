e24158eb4253afd36f7b49703f6423765aad0429
babs105 <babacar.dieng4111@gmail.com>
2024-07-05 12:57:29 +0000

    update 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import locale
-locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+# locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 
 # Fonction pour générer des données simulées
 
